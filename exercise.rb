e8a649df76b743aa0c69736d6a4419f3840a389d
Rafael Mora <rafa.m.mora@gmail.com>
2024-11-24 01:59:15 -0800

    4 runs, 8 assertions, 0 failures, 0 errors, 0 skips!

--- exercise.rb
+++ exercise.rb
@@ -3,7 +3,6 @@ class Exercise
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
-
   def self.marklar(str)
     # TODO: Implement this method
     result = str.scan(/\w+|[^\s\w]/).map do |s|
@@ -20,7 +19,6 @@ class Exercise
   # the "nth" term in the sequence
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
-
   def self.even_fibonacci(nth)
     # TODO: Implement this method
     sum = 0
